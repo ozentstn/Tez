dc2743667a717ec2a0d26a25367103e67f1c7d5e
ozentstn <ozentastan1@gmail.com>
2023-09-13 23:34:18 +0300

    asdsatwrtddfdsfsd

--- main.py
+++ main.py
@@ -1,8 +1,53 @@
-import  bs4
 import nltk
-nltk.download('punkt')
-from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
-sentence="Veri Bilimi Okuluna hoş geldiniz. Bugünkü blog yazısının konusu Natural Language Toolkit"
-print(sent_tokenize(sentence))
-etkisiz_kelimeler = list(stopwords.words('turkish'))
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.decomposition import LatentDirichletAllocation
+
+# Sample text data
+documents = [
+    "Text mining is the process of extracting useful information from text.",
+    "Natural Language Processing (NLP) is a field of study in text mining.",
+    "Machine learning techniques can be applied to text mining tasks.",
+    "NLTK is a popular Python library for natural language processing.",
+    "Scikit-learn is a machine learning library in Python.",
+]
+
+# Step 1: Tokenization and stopwords removal
+nltk.download('stopwords')
+nltk.download('punkt')
+
+stop_words = set(stopwords.words('english'))
+tokenized_documents = [word_tokenize(doc.lower()) for doc in documents]
+filtered_documents = [
+    [word for word in doc if word.isalnum() and word not in stop_words]
+    for doc in tokenized_documents
+]
+
+# Step 2: Feature extraction using TF-IDF vectorization
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_matrix = tfidf_vectorizer.fit_transform([" ".join(doc) for doc in filtered_documents])
+
+# Step 3: Clustering using K-means
+num_clusters = 2  # You can change the number of clusters as needed
+kmeans = KMeans(n_clusters=num_clusters)
+kmeans.fit(tfidf_matrix)
+
+# Step 4: Topic modeling using Latent Dirichlet Allocation (LDA)
+num_topics = 2  # You can change the number of topics as needed
+lda = LatentDirichletAllocation(n_components=num_topics, random_state=42)
+lda.fit(tfidf_matrix)
+
+# Print results
+print("K-means Clusters:")
+for i, doc in enumerate(documents):
+    print(f"Document {i + 1} belongs to cluster {kmeans.labels_[i]}")
+
+print("\nLatent Dirichlet Allocation Topics:")
+for topic_idx, topic in enumerate(lda.components_):
+    print(f"Topic {topic_idx + 1}:")
+    top_words_idx = topic.argsort()[-5:][::-1]
+    top_words = [tfidf_vectorizer.get_feature_names_out()[i] for i in top_words_idx]
+    print(", ".join(top_words))
--- tez.py
+++ tez.py
@@ -12,13 +12,17 @@ import math
 import datetime
 import snowballstemmer
 import spacy
+import tokenize
+from nltk.tokenize import word_tokenize,sent_tokenize
+from nltk.probability import FreqDist
 import docx
+from nltk.tag import pos_tag
 from bs4 import BeautifulSoup
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 from nltk.corpus import wordnet as wn
 from nltk.tokenize import word_tokenize
-from nltk import pos_tag
+from nltk.tag import pos_tag
 from string import punctuation
 import matplotlib.pyplot as plt #kmeans
 import pandas as pd #kmeans
@@ -29,7 +33,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 #Read Word
-file_path="C:\\Users\\ozent\\OneDrive\\Masaüstü\\orhanPamukYeniHayat.docx"
+"""file_path="C:\\Users\\ozent\\OneDrive\\Masaüstü\\orhanPamukYeniHayat.docx"
 def read_docx(file_path):
     document = docx.Document(file_path)
     text = []
@@ -42,28 +46,69 @@ document=read_docx(file_path)
 document_split=document.split(" ")
 vector=pd.Series(document_split)
 document_vector=vector[1:len(vector)] #0 dan başladığı için 1 den başlasın
-document_df=pd.DataFrame(document_vector,columns=["stories"])
+document_df=pd.DataFrame(document_vector,columns=["kelimeler"])
 #küçük harf dönüşümü
 copy_document_df=document_df.copy() #orjinali dursun
-upperLowerCase=copy_document_df["stories"].apply(lambda x:" ".join(x.lower() for x in x.split()))
+copy_document_dff=pd.DataFrame(copy_document_df,columns=["kelimeler"])
+upperLowerCase=copy_document_dff["kelimeler"].apply(lambda x:" ".join(x.lower() for x in x.split()))
+#az kelimelerin silinmesi
+upperLowerCaseDf=pd.DataFrame(upperLowerCase,columns=["kelimeler"])
+delete=pd.Series(''.join(upperLowerCaseDf['kelimeler']).split()).value_counts()<'5'
+azKelime=upperLowerCaseDf['kelimeler'].apply(lambda x: " ".join(x for x in x.split() if x not in delete))
 #sayıların silinmesi
-removeNumbers=[x for x in upperLowerCase if not x.isnumeric()]
+#upperLowerCaseDf=pd.DataFrame(upperLowerCase,columns=["kelimeler"])
+removeNumbers=[x for x in azKelime if not x.isnumeric()]
 #noktalama işaretleri
 noktalama=str.maketrans('','',string.punctuation)
 removeNoktalama=[x.translate(noktalama) for x in removeNumbers]
 #stopwords silinmesi
 sw=stopwords.words("turkish")
 stopWords=[x for x in removeNoktalama if x not in sw]
-#tokenization
+
 #lemmatization: zemberek/ snowballStemmer : kök bulma
 texts=[]
 p_stemmer=PorterStemmer
 tr_stemmer=snowballstemmer.stemmer('turkish')
-"""for i in stopWords:
+for i in stopWords:
    stemmed=[tr_stemmer.stemWord(i) for i in stopWords if not i in sw]
    stemmed_tokens=[i for i in stemmed if not i in sw]
-   #texts.append(stemmed_tokens)"""
-print(stopWords)
+   #texts.append(stemmed_tokens)
+"""
+file_path="C:\\Users\\ozent\\OneDrive\\Masaüstü\\orhanPamukYeniHayat.docx"
+def read_docx(file_path):
+    document = docx.Document(file_path)
+    text = []
+    for paragraph in document.paragraphs:
+        text.append(paragraph.text)
+    return 'n'.join(text)
+document=read_docx(file_path)
+
+def getFreq(content):
+    translator=str.maketrans('','',string.punctuation)
+    #kelime ayrıştırma
+    kelimeler=word_tokenize(content)
+    kelimeler=[word.translate(translator) for word in kelimeler]
+    kelimeler=[word for word in kelimeler if not word.isnumeric()]
+    kelimeler=[word.lower() for word in kelimeler]
+    kelimeler=[word for word in kelimeler if word not in stopwords.words('turkish')]
+    #fdist = FreqDist(kelimeler)
+    return kelimeler
+kelimeler_df=pd.DataFrame(getFreq(document),columns=["Sütun1"])
+fdist = FreqDist(kelimeler_df)
+#ayrık değerlerin çıkarılması
+print(fdist)
+
+#zarf,sıfat
+#nltk.download('averaged_perceptron_tagger')
+#postag=pos_tag(etkisiz_kelimeler)
+
+
+
+
+
+
+
+
 
 
 
